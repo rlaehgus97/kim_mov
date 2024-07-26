6574d217dde6739cef6938d6c7a7a7f2bccb5aa9
rlaehgus97 <starkim625@gmail.com>
2024-07-26 11:24:49 +0900

    base url

---
+++ src/movie/api/call.py
@@ -0,0 +1,9 @@
+def req(dt="20120101"):
+    base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+    key = "1465c9786034ed67b7f78c1dcdea48f6"
+
+    url = f"{base_url}?key={key}&targetDt={dt}"
+    print(url)
+
+req()
+

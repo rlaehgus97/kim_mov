afaae6ec812b41aa5f5d1d64950bc38a81a1e186
rlaehgus97 <starkim625@gmail.com>
2024-07-26 16:30:10 +0900

    read json from api, test code update

--- src/movie/api/call.py
+++ src/movie/api/call.py
@@ -1,12 +1,31 @@
 import requests
+import os    # used for get key method to conceal key value, attain key value from server
+#import pandas as pd
+
+def get_key():
+    key = os.getenv('MOVIE_API_KEY')
+    return key
 
 def gen_url(dt="20120101"):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
-    key = "1465c9786034ed67b7f78c1dcdea48f6"
+    key = get_key()   
     url = f"{base_url}?key={key}&targetDt={dt}"
     
     return url
 
-def req():
-    print("request")
+def req(dt="20120101"):
+    url = gen_url(dt)
+    r = requests.get(url)
+    # 200이 오면 정상, 400이 오면 문제 생김
+    code = r.status_code
+    data = r.json()
+    
+    #print(data)
+    return code, data
 
+def req2dataframe():
+    _, data = req()
+    l = data['boxOfficeResult']['dailyBoxOfficeList']
+    return l
+
+print(req2dataframe())
--- tests/test_call.py
+++ tests/test_call.py
@@ -1,7 +1,23 @@
-from movie.api.call import gen_url
+from movie.api.call import gen_url, req, get_key, req2dataframe
+
+def test_get_key():
+    key = get_key()
+    assert key
 
 def test_gen_url():
     url = gen_url()
-    assert True
+
     assert "http" in url
     assert "kobis" in url
+
+def test_req():
+    code, data = req()
+    assert code == 200
+    
+def test_req2dataframe():
+    l = req2dataframe()
+    assert len(l) > 0
+    v = l[0]
+    assert 'rnum' in v.keys()
+    #assert type(req2dataframe()) is list
+    assert v['rnum'] == '1'
